54198f340cb5981bb62a0e60f5200f181f900495
Polina <smivasilisa@gmail.com>
2024-05-01 19:40:48 +0200

    Alter table

--- postgres-students.session.sql
+++ postgres-students.session.sql
@@ -1,13 +1,35 @@
-CREATE TABLE coordinates(
-  x int,
-  y int,
-  z int,
-  CONSTRAINT "unique_coord" PRIMARY KEY(x, y, z)
+CREATE TABLE products(
+  id serial PRIMARY KEY,
+  brand varchar(200) NOT NULL CHECK (brand != ''),
+  model varchar(300) NOT NULL CHECK (model != ''),
+  description text,
+  cathegory varchar(200) NOT NULL CHECK (cathegory != ''),
+  price numeric(10, 2) NOT NULL CHECK (price > 0),
+  discounted_price numeric(10,2) CHECK (discounted_price <= price)
 );
 
-INSERT INTO coordinates VALUES
-(111,22,36),
-(252,76,88);
+DROP TABLE products;
 
-INSERT INTO coordinates VALUES
-(111,22,36);--error
+INSERT INTO products(brand, model, cathegory, price) VALUES
+('Samsung', 'S10', 'smartphones', 200),
+('IPhone', '15 Pro', 'smartphones', 1200),
+('LG', '1234trc', 'TV', 300),
+('Sony', '457', 'TV', 600);
+
+INSERT INTO products(brand, model, cathegory, price) VALUES
+('IPhone', '15 Pro', 'smartphones', 500);
+
+ALTER TABLE products
+ADD CONSTRAINT "unique_brand_model_pair" UNIQUE(brand, model);
+
+ALTER TABLE products
+ADD COLUMN quantity int;
+
+ALTER TABLE products
+ADD CONSTRAINT "products_quantity_check" CHECK(quantity >= 0);
+
+ALTER TABLE products
+DROP CONSTRAINT "products_quantity_check";
+
+ALTER TABLE products
+DROP COLUMN quantity;
