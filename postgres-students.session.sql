924de67ee8d584629a60a9a619b25927958e4f8a
Polina <smivasilisa@gmail.com>
2024-05-01 14:20:22 +0200

    Creating table

--- postgres-students.session.sql
+++ postgres-students.session.sql
@@ -1,1 +1,9 @@
-CREATE TABLE test();
+CREATE TABLE users_with_email();
+
+DROP TABLE test, users_with_email, users;
+
+CREATE TABLE users(
+  first_name varchar(64),
+  last_name varchar(64),
+  biography text
+);
