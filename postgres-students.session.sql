43b1110d3a3deb1a639c798c82da8ff71d776fbc
Polina <smivasilisa@gmail.com>
2024-05-01 17:06:19 +0200

    Inserting data into table

--- postgres-students.session.sql
+++ postgres-students.session.sql
@@ -10,3 +10,11 @@ CREATE TABLE  users(
   foot_size smallint,
   height numeric(5, 2)
 );
+
+INSERT INTO users VALUES('John', 'Doe', 'Tady je hodne textu', 'male', true, '2004-09-14', 43, 1.75);
+
+INSERT INTO users VALUES
+('Susan', 'Doe', 'Tady je hodne textu o Susan', 'female', true, '1994-09-14', 40, 1.65),
+('Peter', 'Doe', 'Tady je hodne textu o Peteru', 'male', true, '1990-09-14', 46, 1.95);
+
+INSERT INTO users VALUES('Blake', 'Doe', 'Tady je hodne textu o Blaku', 'male', false, '2004-09-14', NULL, NULL);
