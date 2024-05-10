8bbecb5aced8fa292094faa622c37f3755788257
Polina <smivasilisa@gmail.com>
2024-05-10 19:26:43 +0200

    Views

---
+++ conditions-and-views.sql
@@ -0,0 +1,39 @@
+SELECT u.id, u.first_name, u.last_name, u.email, count(o.id) AS "orders_amount" FROM
+users AS u LEFT JOIN orders AS o
+ON u.id = o.customer_id
+GROUP BY u.id
+ORDER BY "orders_amount";
+
+CREATE OR REPLACE VIEW users_with_orders_amount AS (
+  SELECT u.id, u.first_name, u.last_name, u.email, count(o.id) AS "orders_amount" FROM
+  users AS u LEFT JOIN orders AS o
+  ON u.id = o.customer_id
+  GROUP BY u.id
+  ORDER BY "orders_amount"
+);
+
+SELECT * FROM users_with_orders_amount;
+
+-------------------------------------------
+
+SELECT email, orders_amount FROM users_with_orders_amount
+WHERE orders_amount < 2;
+
+CREATE OR REPLACE VIEW orders_with_price AS (
+SELECT o.id, o.customer_id, sum(p.price * otp.quantity) AS "order_sum", o.status FROM
+orders AS o JOIN orders_to_products AS otp
+ON o.id = otp.order_id
+JOIN products AS p
+ON p.id = otp.product_id
+GROUP BY o.id
+);
+
+DROP VIEW orders_with_price;
+
+-------------------------------------------
+
+SELECT u.id, u.email, sum(owp.order_sum) AS "sum" FROM 
+users AS u LEFT JOIN orders_with_price AS owp
+ON u.id = owp.customer_id
+GROUP BY u.id
+ORDER BY "sum";
