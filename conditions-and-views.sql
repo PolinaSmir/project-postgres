f3ded13b4bc6b4bfefb1cecc2b9ccea7e9f822b4
Polina <smivasilisa@gmail.com>
2024-05-19 13:53:17 +0200

    Create schema

--- conditions-and-views.sql
+++ conditions-and-views.sql
@@ -1,27 +1,7 @@
-SELECT * FROM orders
-WHERE status = true;
+CREATE SCHEMA new_schema;
 
-INSERT INTO orders(customer_id, status) VALUES
-(3994, 'new');
-
-CREATE TYPE order_status AS ENUM('new', 'processing', 'shiped', 'done', 'canceled');
-
-ALTER TABLE orders
-ALTER COLUMN status
-TYPE order_status
-USING (
-  CASE status
-  WHEN false THEN 'processing'
-  WHEN true THEN 'done'
-  ELSE 'new'
-  END
-)::order_status;
-
-DROP VIEW users_with_orders_amount, orders_with_price;
-
-SELECT * FROM orders
-ORDER BY created_at DESC;
-
-UPDATE orders
-SET status = 'done'
-WHERE id = 14083;
+CREATE TABLE new_schema.users(
+  first_name VARCHAR(20) NOT NULL CHECK(first_name != ''),
+  last_name VARCHAR(20) NOT NULL CHECK(last_name != ''),
+  email VARCHAR(20) NOT NULL CHECK(email != '')
+);
