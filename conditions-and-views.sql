7059e2cd3f0bce8b05049f2a432a689cb56620f8
Polina <smivasilisa@gmail.com>
2024-05-19 12:22:23 +0200

    Materialized views

--- conditions-and-views.sql
+++ conditions-and-views.sql
@@ -1,61 +1,29 @@
-SELECT u.id, u.first_name, u.last_name, u.email, count(o.id) AS "orders_amount" FROM
-users AS u LEFT JOIN orders AS o
-ON u.id = o.customer_id
-GROUP BY u.id
-ORDER BY "orders_amount";
-
-CREATE OR REPLACE VIEW users_with_orders_amount AS (
-  SELECT u.id, u.first_name, u.last_name, u.email, count(o.id) AS "orders_amount" FROM
-  users AS u LEFT JOIN orders AS o
-  ON u.id = o.customer_id
-  GROUP BY u.id
-  ORDER BY "orders_amount"
+CREATE MATERIALIZED VIEW total_orders AS (
+  SELECT count(*) AS "общее кол-во заказов" FROM orders
 );
 
-SELECT * FROM users_with_orders_amount;
+DROP MATERIALIZED VIEW total_orders;
 
--------------------------------------------
+SELECT * FROM total_orders; --8072
 
-SELECT email, orders_amount FROM users_with_orders_amount
-WHERE orders_amount < 2;
+INSERT INTO orders (customer_id, status)
+VALUES (
+    2732,
+    false
+  );
 
-CREATE OR REPLACE VIEW orders_with_price AS (
-SELECT o.id, o.customer_id, sum(p.price * otp.quantity) AS "order_sum", o.status FROM
-orders AS o JOIN orders_to_products AS otp
-ON o.id = otp.order_id
-JOIN products AS p
-ON p.id = otp.product_id
-GROUP BY o.id
-);
+REFRESH MATERIALIZED VIEW total_orders;
 
-DROP VIEW orders_with_price;
-
--------------------------------------------
-
-SELECT u.id, u.email, sum(owp.order_sum) AS "sum" FROM 
-users AS u LEFT JOIN orders_with_price AS owp
-ON u.id = owp.customer_id
-GROUP BY u.id
-ORDER BY "sum";
-
--- Создание функции 
-
-CREATE FUNCTION add_numbers(number1 integer, number2 integer)
-RETURNS integer
+CREATE FUNCTION refresh_materialized_view()
+RETURNS void
 AS
 $$
 BEGIN
-  RETURN number1*number2;
-END;
+  REFRESH MATERIALIZED VIEW total_orders;
+END
 $$
 LANGUAGE plpgsql;
 
--- Вызов функции
+DROP FUNCTION refresh_materialized_view();
 
-SELECT add_numbers('5', 15);
-
--- Удаление функции
-
-DROP FUNCTION add_numbers;
-
-DROP FUNCTION add_numbers(integer, integer);-- Правильный вызов функции
+SELECT refresh_materialized_view();
